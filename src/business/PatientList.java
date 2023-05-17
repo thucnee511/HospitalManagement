48edeebace9abe1bd11132ee5b8ee2f11303fc29
ngocngocthuc@gmail.com <ngocngocthuc@gmail.com>
2023-05-18 04:54:18 +0700

    update

--- src/business/PatientList.java
+++ src/business/PatientList.java
@@ -1,6 +1,5 @@
 package business;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 
--- src/tools/InputHandle.java
+++ src/tools/InputHandle.java
@@ -8,8 +8,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
